cf85dba89ef9afa7d83fab615f2e832a84f844ba
Jasson <jasson.cordones@hotmail.com>
2024-10-12 18:32:11 -0400

    Added place holder logo and favicon

--- javascript/main.js
+++ javascript/main.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const courseCard = document.createElement('div');
             courseCard.classList.add('course-card');
             courseCard.innerHTML = `
-            <img src="${course.logoUrl}" alt="Organization Logo">
+            <img src="${course.logoUrl}" alt="Organization Logo" width="64px" height="64px">
             <div class="course-details">
             <h3>${course.name}</h3>
             <p><strong>Issuing Organization:</strong> ${course.org}</p>
